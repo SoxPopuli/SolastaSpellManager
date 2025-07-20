86bca9efc65706f55ffd115c60495198db609184
Charlotte Smith <charliesmith5019@gmail.com>
2025-07-20 23:42:17 +0100

    Add metamagic on items patch

---
+++ SolastaSpellManager/Main.fs
@@ -0,0 +1,42 @@
+﻿module SolastaSpellManager.Main
+
+open HarmonyLib
+open UnityEngine
+open UnityModManagerNet
+
+#if DEBUG
+let () = Harmony.DEBUG <- true
+#endif
+
+let harmony = Harmony("com.soxpopuli.solastasavemanager")
+
+let onToggle (modEntry: UnityModManager.ModEntry) (value: bool) =
+    true
+
+let load (modEntry: UnityModManager.ModEntry) =
+    modEntry.OnToggle <- onToggle
+
+
+    harmony.PatchAll()
+
+    true
+
+
+[<HarmonyPatch(typeof<RulesetCharacter>)>]
+[<HarmonyPatch("UseDeviceFunction")>]
+type MetamagicForItemSpells() =
+    static member Prefix(__instance: RulesetCharacter, usableDevice: RulesetItemDevice, ``function``: RulesetDeviceFunction, additionalCharges: int) =
+        let isSpell = ``function``.DeviceFunctionDescription.Type = DeviceFunctionDescription.FunctionType.Spell
+
+        if isSpell then
+            let service = ServiceRepository.GetService<IRulesetImplementationService>()
+            let spellDef = ``function``.DeviceFunctionDescription.SpellDefinition
+            let spellEffect = RulesetEffectSpell(__instance, spellDef)
+
+            if service.IsAnyMetamagicOptionAvailable(spellEffect, __instance) then
+                let meta = Gui.GuiService.GetScreen<MetamagicSelectionPanel>()
+                let character = GameLocationCharacter.GetFromActor(__instance)
+                meta.Bind(character, spellEffect, meta.MetamagicOptionSelected, meta.MetamagicOptionIgnored)
+                meta.Show()
+
+        ()
